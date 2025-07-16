47ce07e1f2b66411036dae149e89109d974042a1
tenchd <dtench@pm.me>
2025-07-16 07:22:11 -0700

    breaking example

--- include/example.h
+++ include/example.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "cxx-test/src/main.rs.h"
 #include "rust/cxx.h"
+#include <iostream>
 
 rust::Vec<Shared> f(rust::Vec<Shared> elements);
+
+/*
+void stupid() {
+    std::cout << "you are not stupid" << std::endl;
+}
+*/
