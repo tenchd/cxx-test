49c2f9de3d3e55ca5fde6e100a194718ba912281
David Tench <dtench@lbl.gov>
2025-07-22 12:06:10 -0700

    jl sketch

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use cxx::Vector;
 use sprs::{CsMat};
+extern crate fasthash;
 
 mod utils;
 use utils::read_mtx;
 
-use crate::ffi::FlattenedVec;
+mod jl_sketch;
+use jl_sketch::{jl_sketch_sparse,jl_sketch_sparse_blocked};
 
-//use crate::ffi::no_sharing;
+use crate::ffi::FlattenedVec;
 
 #[cxx::bridge]
 mod ffi {
@@ -35,7 +37,7 @@ mod ffi {
 }
 
 // reads a jl_sketch vec of vecs from a file and 
-fn read_vecs_from_file_flat(filename: &str) -> ffi::FlattenedVec {
+fn read_vecs_from_file_flat(filename: &str) -> FlattenedVec {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
@@ -65,18 +67,34 @@ fn read_vecs_from_file_flat(filename: &str) -> ffi::FlattenedVec {
 
 
 // currently assumes that you don't need to manage diagonals of input matrix. fix this later
-fn precondition_and_solve(input_filename: &str, sketch_filename: &str) -> FlattenedVec {
+fn precondition_and_solve(input_filename: &str, sketch_filename: &str, seed: u64, jl_factor: f64, jl_dim: usize, block_rows: usize, block_cols: usize, display:bool) -> FlattenedVec {
     //let filename = "data/fake_jl_multi.csv".to_string();
 
-    let shared_jl_cols_flat = read_vecs_from_file_flat(sketch_filename);
-    let m = shared_jl_cols_flat.num_cols;
-    let n = shared_jl_cols_flat.num_rows;
-
     let input_csc = read_mtx(input_filename);
-    
+
+    //make sure diagonals are nonzero; generalize this later
     for i in input_csc.diag_iter(){
         assert!(*i.unwrap() != 0.0 as f64)
     }
+
+    let n: usize = input_csc.cols();
+    let m: usize = input_csc.rows();
+
+    let seed: u64 = 1;
+    let jl_factor: f64 = 1.5;
+    let jl_dim = ((n as f64).log2() *jl_factor).ceil() as usize;
+    println!("output matrix is {}x{}", n, jl_dim);
+
+    // not use at the moment; need to convert it to flattenedvec so i can pass it to c++. try the to_dense function maybe?
+    let mut sketch_sparse_format: CsMat<f64> = CsMat::zero((jl_dim,n)).transpose_into();
+
+    jl_sketch_sparse_blocked(&input_csc, &mut sketch_sparse_format, jl_dim, seed, block_rows, block_cols, display);
+    
+    let shared_jl_cols_flat = read_vecs_from_file_flat(sketch_filename);
+    let m = shared_jl_cols_flat.num_cols;
+    let n = shared_jl_cols_flat.num_rows;    
+
+
 
     let temp_input_col_ptrs = input_csc.indptr().as_slice().unwrap().to_vec();
     let input_col_ptrs: Vec<i32> = temp_input_col_ptrs.into_iter().map(|x| x as i32).collect();
@@ -94,35 +112,19 @@ fn precondition_and_solve(input_filename: &str, sketch_filename: &str) -> Flatte
 
 fn main() {
 
+    let seed: u64 = 1;
+    let jl_factor: f64 = 1.5;
+    let jl_dim = ((n as f64).log2() *jl_factor).ceil() as usize;
+    let block_rows: usize = 100;
+    let block_cols: usize = 15000;
+    let display: bool = false;
+    println!("output matrix is {}x{}", n, jl_dim);
+
     let sketch_filename = "data/fake_jl_multi.csv";
     let input_filename = "/global/u1/d/dtench/cholesky/Parallel-Randomized-Cholesky/physics/parabolic_fem/parabolic_fem-nnz-sorted.mtx";
 
     let solution = precondition_and_solve(input_filename, sketch_filename);
 
     println!("solution has {} cols, {} rows, and initial value {}", solution.num_cols, solution.num_rows, solution.vec[0]);
-
-
-    // let shared_jl_cols_flat = read_vecs_from_file_flat(filename);
-    // let m = shared_jl_cols_flat.num_cols;
-    // let n = shared_jl_cols_flat.num_rows;
-
-    // let physics_csc = read_mtx("/global/u1/d/dtench/cholesky/Parallel-Randomized-Cholesky/physics/parabolic_fem/parabolic_fem-nnz-sorted.mtx");
-    
-    // for i in physics_csc.diag_iter(){
-    //     assert!(*i.unwrap() != 0.0 as f64)
-    // }
-
-    // let temp_phys_col_ptrs = physics_csc.indptr().as_slice().unwrap().to_vec();
-    // let phys_col_ptrs: Vec<i32> = temp_phys_col_ptrs.into_iter().map(|x| x as i32).collect();
-    // let temp_phys_row_indices = physics_csc.indices().to_vec();
-    // let phys_row_indices: Vec<i32> = temp_phys_row_indices.into_iter().map(|x| x as i32).collect();
-    // let phys_values = physics_csc.data().to_vec();
-    // //let phys_values = temp_phys_values.into_iter().map(|x| x as u64).collect();
-    // println!("phys col_ptrs size in rust: {:?}. first value: {}", phys_col_ptrs.len(), phys_col_ptrs[0]);
-    // println!("phys row_indices size in rust: {:?}. first value: {}", phys_row_indices.len(), phys_row_indices[0]);
-    // println!("phys values size in rust: {:?}. first value: {}", phys_values.len(), phys_values[0]);
-    // println!("nodes in phys csc: {}, {}", physics_csc.cols(), physics_csc.rows());
-    // //ffi::sprs_correctness_test(phys_col_ptrs, phys_row_indices, phys_values);
-    // ffi::run_solve_lap(shared_jl_cols_flat, phys_col_ptrs, phys_row_indices, phys_values);
 
 }
